8d4b7deab901e239165c3cf1fd975edea82e7d1b
Pierre Villemot <tiky.halbaroth@gmail.com>
2023-03-29 15:17:06 +0200

    Attempt to fix path

--- src/core/Run_proc.ml
+++ src/core/Run_proc.ml
@@ -17,7 +17,7 @@ let run cmd : Run_proc_result.t =
   (* call process and block *)
   let p =
     let cmd =
-      Format.sprintf "_build/install/default/bin/wrapper \
+      Format.sprintf "wrapper \
         --show-output -- %s" cmd in
     try
       let oc, ic, errc = Unix.open_process_full cmd (Unix.environment()) in
